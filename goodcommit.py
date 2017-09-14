ed35653b36d2075c447c444863b5e27f3cfc3c73
developer <developer@Andyde-MacBook-Pro.local>
2017-09-14 23:10:16 +0800

    add argument

--- goodcommit.py
+++ goodcommit.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 import os
 import subprocess
+import sys
 
 localFile = []
 remoteFile = []
@@ -10,10 +11,14 @@ remoteFile = []
 watchList = dict()
 diffList = dict()
 reviewerList = []
+reviewDetailList = []
 testplanList = []
+topdir = ''
 
 def findWatchers(fileName):
-	fp = open(fileName)
+	global topdir
+	#print('findWatchers: ', topdir + '/' + fileName)
+	fp = open(topdir + '/' + fileName)
 	watchList[fileName] = []
 	lineNum = 0
 	for line in fp:
@@ -49,6 +54,7 @@ def findWatchers(fileName):
 
 def findDiffList(lines):
 
+	global topdir
 	for line in lines:
 		#print(line)
 		diffs = line.split()
@@ -70,10 +76,10 @@ def findDiffList(lines):
 
 	if localFile == []:
 		print('no different')
-		exit()
+		sys.exit(0)
 	
 	print('checking ', localFile)
-	
+
 	for item in localFile:
 		#print('local file :', item)
 		findWatchers(item)
@@ -86,52 +92,92 @@ def findDiffList(lines):
 
 def findReviewerAndTestPlan():
 	for fn in localFile:
-		#print('file', fn)
+		print('file', fn)
 		for i, diffs in enumerate(diffList[fn]):
-			#print('\tdiff ', diffs)
+			print('\tdiff ', diffs)
 			for j, watcher in enumerate(watchList[fn]):
-				#print('\t\tWatcher: ', watcher[3], watcher[4])
+				print('\t\tWatcher: ', watcher[3], watcher[4])
 				checkrange = range(watcher[3], watcher[4])
 				if (diffs[0] in checkrange) or (diffs[1] in checkrange):
-					#print('\t\t\tmatched')
+					print('\t\t\tmatched')
 					reviewerList.append(watcher[1][0])
+					reviewDetailList.append([watcher[1][0], fn, checkrange])
 					testplanList.append(watcher[2][0])
 
 def parseGitBlame():
 
 	for file in remoteFile:
 		#print('execute git blame', file)
-		blameOutput = subprocess.check_output(['git', 'blame', file])
+		global topdir
+		blameOutput = subprocess.check_output(['git', 'blame', '-f', topdir + '/' + file])
 		#print(blame)
 		blamLines = blameOutput.splitlines()
+		lastReviewInfo = []
 		for item in diffList[file]:
 			for line in range(item[0], item[1]):
 				#print(blamLines[line])
 				commitId = blamLines[line].split()[0]
-				author = blamLines[line].split()[1].lstrip('(')
+				author = blamLines[line].split()[2].lstrip('(')
 				if commitId != '00000000':
 					#print('blame ', commitId, ' ', author)
 					reviewerList.append(author)
+					reviewinfo = [author, file, commitId, [item[0], item[1]]]
+					if (lastReviewInfo != reviewinfo):
+						reviewDetailList.append(reviewinfo)
+					lastReviewInfo = reviewinfo
+
 
 
 def main():
 
-	cmd = 'git diff'
+	from optparse import OptionParser
+
+	
+	parser = OptionParser()
+	parser.add_option("-b", "--blame",
+					action="store_true", dest="blame", default=False,
+					help="run git blame to find the reviewer")
+	parser.add_option("-d", "--detail",
+					action="store_true", dest="detail", default=False,
+					help="print details")
+
+	(options, args) = parser.parse_args()
+	
+
+	global topdir
+	
 	try:
 		diff_output = subprocess.check_output(['git', 'diff', '--staged'])
 	except:
 		print('error to execute')
-		exit()
-		
+		sys.exit(0)
+
+	topdir = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip('\n')
+	print('top dir', topdir)
 	diff_lines = diff_output.splitlines()
 	findDiffList(diff_lines)
 	findReviewerAndTestPlan()
-	parseGitBlame()
 	
-	print('reviews: ')
+	if options.blame == True:
+		parseGitBlame()
+	
+	
+	print('\n\n------------ result ------------');
+	
+	if options.detail == True:
+		print('reviews detail: ')
+		for name in reviewDetailList:
+			print('\t', name)
+			logs = subprocess.check_output(['git', 'log', '-1', name[2]])
+			#print(logs)
+			logs = logs.splitlines()
+			print('\t\t', logs[4])
+	
+	print('reviewer : ')
 	for name in set(reviewerList):
 		print('\t', name)
-	
+
+		
 	print('test plan: ')
 	for name in set(testplanList):
 		print('\t', name)
